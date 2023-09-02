6d0d24e970edb3cf471a904f79083d12b58f7df4
Emma Anderson <workeandrs@gmail.com>
2023-09-01 21:35:58 -0700

    2023-09-01T21:35:58-07:00

--- linkie.sh
+++ linkie.sh
@@ -6,6 +6,24 @@ case ${LINKIE_REPO} in
   *)  echo "=> LINKIE_REPO: [${LINKIE_REPO}]" ;;
 esac
 
+case "${0}" in *glinkie*)
+  case "${1}" in
+    sync|"")
+      cd "${GLINKIE_REPO}"
+      dconf dump /org/gnome/ > gnome.ini
+      git add *
+      if git commit -m `date -Is`; then git push; fi
+    ;;
+    restore)
+      dconf load /org/gnome/ < gnome.ini
+    ;;
+    help)
+      printf "add the following to your crontab.\n* * * * * $(realpath ${0}) sync\n"
+    ;;
+  esac
+  exit 0
+esac
+
 case ${LINKIE_PATH} in
   "") echo "=> [export a LINKIE_PATH (e.g. ~/.config) and try again]"; exit 1 ;;
   *)  echo "=> LINKIE_PATH: [${LINKIE_PATH}]" ;;
